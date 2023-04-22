e6626a949884fdb4c3cf4ed14b173fe2c871b00a
Matthew Brett <matthew.brett@gmail.com>
2023-04-22 22:29:44 +0100

    Refactor to use unscrewed

--- nipraxis/tests/test_local_cache.py
+++ nipraxis/tests/test_local_cache.py
@@ -6,6 +6,8 @@ from hashlib import sha1
 
 import nipraxis._fetcher as npxf
 
+import unscrewed
+
 CAMERA_HASH = 'af7257977f30797d4b3ea7dd15fa362d4fe8c37e'
 
 
@@ -20,7 +22,7 @@ def test_camera(tmp_path, monkeypatch):
     staging_cache = tmp_path / 'nipraxis-staging'
     monkeypatch.delenv("NIPRAXIS_STAGING_CACHE", raising=False)
     monkeypatch.setenv("NIPRAXIS_LOCAL_CACHE", str(local_cache))
-    fetcher = npxf.Fetcher(npxf._CONFIG)
+    fetcher = unscrewed.Fetcher(npxf._config_file)
     monkeypatch.setattr('nipraxis._fetcher.fetch_file', fetcher.fetch_file)
     fname = npxf.fetch_file('camera.txt')
     assert fname.startswith(str(local_cache))
