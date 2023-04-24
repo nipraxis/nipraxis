d38bda273c9eece4597066704f94284a182474c8
Matthew Brett <matthew.brett@gmail.com>
2023-04-24 12:31:08 +0100

    Make fetcher public

--- nipraxis/_fetcher.py
+++ nipraxis/_fetcher.py
@@ -6,5 +6,6 @@ import pkg_resources
 import unscrewed
 
 _config_file = pkg_resources.resource_filename("nipraxis", "registry.yaml")
-_fetcher = unscrewed.Fetcher(_config_file)
-fetch_file = _fetcher.fetch_file
+
+fetcher = unscrewed.Fetcher(_config_file)
+fetch_file = fetcher.fetch_file
